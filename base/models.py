bdc5ff00b7f293c55d2166152c5b97e34c3eace9
drq <1729866827@qq.com>
2024-04-29 16:31:02 +0800

    init

---
+++ base/models.py
@@ -0,0 +1,56 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+# Create your models here.
+class Student(models.Model):
+    student_id = models.CharField(max_length=32)
+    password = models.CharField(max_length=64)
+    email = models.CharField(max_length=64)
+    name = models.CharField(max_length=32, blank=True)
+    phone = models.CharField(max_length=32, blank=True)
+    academy = models.IntegerField(blank=True)
+    avatar = models.CharField(max_length=200, blank=True)
+
+    class Meta:
+        db_table = 'Student'
+
+
+class Admin(models.Model):
+    staff_id = models.CharField(max_length=32)
+    password = models.CharField(max_length=64)
+    email = models.CharField(max_length=64)
+    name = models.CharField(max_length=32)
+    avatar = models.CharField(max_length=200)
+
+    class Meta:
+        db_table = 'Admin'
+
+
+class Notification(models.Model):
+    notification_id = models.CharField(max_length=32)
+    student = models.ForeignKey(to="Student", on_delete=models.CASCADE)
+    reason = models.CharField(max_length=64)
+    time_slot = models.TimeField()
+    read = models.BooleanField()
+    admin = models.ForeignKey(to="Admin", on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = "Notification"
+
+
+class Picture(models.Model):
+    image = models.CharField(max_length=200)
+
+    class Meta:
+        db_table = 'Picture'
+
+
+class Push(models.Model):
+    push_id = models.IntegerField()
+    title = models.CharField(max_length=64)
+    pre_content = models.CharField(max_length=64)
+    picture = models.CharField(max_length=200)
+
+    class Meta:
+        db_table = 'Push'
---
+++ order/views.py
@@ -0,0 +1,86 @@
+import json
+from .lib import time
+from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse, HttpResponse
+from .models import Place, Order, Team, TeamMember
+from manager.models import Lecturer, LecturerPlace
+
+
+# from ..manager.models import Lecturer, LecturerPlace
+
+
+# Create your views here.
+
+def person(request):
+    data = json.loads(request.body.decode('utf-8'))
+    week_num, time_index = time.trans_index(data['time_index'])
+    place = Place.objects.filter(week_num=week_num, time_index=time_index).first()
+    if place['capacity'] > count_order(place['id']):
+        Order.objects.create(place=place, user_id=data['id'], name=data['name'], phone=data['phone'],
+                             academy=data['academy'], is_person=True)
+        return JsonResponse({
+            'success': True
+        })
+    else:
+        return JsonResponse({
+            'success': False
+        })
+
+
+def group(request):
+    data = json.loads(request.body.decode('utf-8'))
+    week_num, time_index = time.trans_index(data['time_index'])
+    place = Place.objects.filter(week_num=week_num, time_index=time_index).first()
+    if place['capacity'] > count_order(place['id']):
+        team = Team.objects.create(leader_name=data['leader']['name'], leader_id=data['leader']['id'],
+                                   leader_phone=data['leader']['phone'], academy=data['academy'])
+        persons = data['persons']
+        for person in persons:
+            TeamMember.objects.create(team=team, member_id=person['id'], member_name=person['name'])
+        Order.objects.create(place=place, user_id=data['id'], name=data['name'], phone=data['phone'],
+                             academy=data['academy'], is_person=False, team=team)
+        return JsonResponse({
+            'success': True
+        })
+    else:
+        return JsonResponse({
+            'success': False
+        })
+
+
+def get_info(request):
+    details = []
+    for time_index in (1, 28):
+        week_num, new_time_index = time.trans_index(time_index)
+        place = Place.objects.filter(week_num=week_num, time_index=new_time_index).first()
+        capacity = 20
+        enrolled = count_order(place['id'])
+        lecturer = ""
+        lecturer_with_place = place.lecturerPlace_set.all()
+        for l in lecturer_with_place:
+            lecturer += Lecturer.objects.filter(id=l['lecturer_id']).first()['name']
+        if enrolled == 0:
+            type = 0
+        elif capacity == enrolled:
+            type = 2
+        else:
+            type = 1
+        de = {
+            "time_index": time_index,
+            "capacity": capacity,
+            "enrolled": enrolled,
+            "lecturer": lecturer,
+            "type": type
+        }
+        details.append(de)
+    return JsonResponse(details)
+
+
+def count_order(place_id):
+    count = Order.objects.filter(place_id=place_id, is_person=True).count()
+    order_with_team = Order.objects.filter(place_id=place_id, is_person=False).select_related('team').first()
+    team_with_member = TeamMember.objects.select_related('team').filter(team_id=order_with_team['team_id']).all()
+    count += len(team_with_member)
+    return count
+
